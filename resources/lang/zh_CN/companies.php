87948a02cec46a33676243b1bdf0980219261c72
jxintao <763421217@qq.com>
2024-04-19 10:53:42 +0800

    first commit

---
+++ resources/lang/zh_CN/companies.php
@@ -0,0 +1,12 @@
+<?php 
+return [
+    'labels' => [
+        'Companies' => '客户',
+        'companies' => '客户',
+    ],
+    'fields' => [
+        'name' => 'name',
+    ],
+    'options' => [
+    ],
+];
